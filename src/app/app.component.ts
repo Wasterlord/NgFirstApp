0817dcc98babf81ec3eb2f56d6d8034d98775dd3
Dmitry <godwasterlord@gmail.com>
2018-01-09 12:22:19 +0100

    first commit for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  
+  users = [
+    {name: "WFM 1"},
+    {name: "WFM 2"},
+    {name: "WFM 3"},
+    {name: "WFM 4"},
+    {name: "WFM 5"},
+    {name: "WFM 6"},
+    {name: "WFM 7"},
+    {name: "WFM 8"}
+
+  ]
 }
